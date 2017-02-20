481edcac41db0557ee6894b84d07e6184a762321
Darlan Mendonça <darlanmendonca@gmail.com>
2017-02-20 14:29:47 -0300

    continue tests with mask

--- sources/scripts/mn-number.class.js
+++ sources/scripts/mn-number.class.js
@@ -38,18 +38,56 @@ class MnNumber extends window.MnInput {
         ? Number(this.getAttribute('decimal'))
         : 2
 
+      const mask = document.createElement('div')
+      mask.classList.add('mask')
+      this.appendChild(mask)
+
+      const value = Number(this.value).toFixed(precision)
+      this.value = value
+      mask.setAttribute('value', input.value.replace('.', ','))
+      this.classList.add('has-value')
+
+      input.addEventListener('keydown', () => {
+        mask.setAttribute('value', input.value.replace('.', ','))
+      })
+
+      input.addEventListener('keyup', () => {
+        mask.setAttribute('value', input.value.replace('.', ','))
+
+        if (input.value.length) {
+          this.classList.add('has-value')
+        } else {
+          this.classList.remove('has-value')
+        }
+      })
+
       input.addEventListener('change', () => {
         if (this.value) {
           const value = Number(this.value).toFixed(precision)
           this.value = 0
           this.value = value
+          mask.setAttribute('value', input.value.replace('.', ','))
           this.setAttribute('value', value)
           // mask.value = value + ' %'
 
           // console.log('this.value', this.value)
         }
       })
+    } else {
+      input.addEventListener('keydown', event => {
+        if (event.key === ',') {
+          event.preventDefault()
+        }
+      })
 
+      input.addEventListener('paste', event => {
+        event.preventDefault()
+        const value = event.clipboardData.getData('Text')
+        const number = parseInt(value)
+        if (!isNaN(number)) {
+          this.value = number
+        }
+      })
     }
 
     return self
